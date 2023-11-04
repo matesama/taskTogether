0b59b47a6f873f46655c33f82232f2119de47e7e
Mateo Santa <mateo.santamanrique@gmail.com>
2023-11-04 17:15:01 +0100

    workin context api

--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 
 const loginCall = async (userCredential, dispatch) => {
-    
+
     try{
-        dispatch({type: LOGIN_START});
+        dispatch({type: "LOGIN_START"});
+        console.log("test")
         const response = await axios.post("http://localhost:8000/api/auth/login", userCredential)
+        console.log(response);
         dispatch({type: "LOGIN_SUCCESS", payload: response.data })
+        
     } catch(err){
         dispatch({type: "LOGIN_FAILURE", payload: err })
     }
