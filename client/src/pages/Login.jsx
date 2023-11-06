9e25db55d6bdd7d7552004f733f7b9bdf04da39b
Mateo Santa <mateo.santamanrique@gmail.com>
2023-11-06 21:38:06 +0100

    register and login responsiveness

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -33,21 +33,23 @@ const Login = () => {
     }
 
     return(
-        <div className="text-slate-300 flex flex-row">
-            <div className="w-6/12  min-h-screen bg-slate-950 min-w-screen flex flex-col justify-between" >
+        <div className="text-slate-300 flex  lg:flex-row">
+            <div className="max-lg:invisible w-6/12 max-lg:w-0  min-h-screen bg-slate-950 min-w-screen flex flex-col justify-between" >
                 <h2 className="font-bold text-2xl text-start pl-5 pt-5 mt-5 ml-5 w-8/12">Set Goals, Stay Accountable, Succeed Together</h2>
                 <h2 className="font-bold mb-5 ml-5 text-2xl self-start pl-5 pb-5">taskTogether</h2>
             </div>
-            <form onSubmit={getData} className="bg-slate-800 w-6/12 m-0 min-h-screen min-w-screen flex justify-center items-center">
-                <div className="flex flex-col  m-0 items-center rounded-md border-slate-300 border-solid border-2 text-slate-300 p-12">
-                    <h2 className="font-bold mb-5 text-3xl">Sign In</h2>
-                    <div className="flex flex-col">
-                        <label htmlFor="email" className="text-sm text-start mb-0">Email</label>
+            <form onSubmit={getData} className="bg-slate-800 w-6/12 max-lg:w-screen m-0 min-h-screen min-w-screen flex max-lg:flex-col justify-center items-center">
+                <h2 className=" lg:invisible lg:w-0 font-bold  text-2xl mb-4">taskTogether</h2>
+                <h4 className=" lg:invisible lg:w-0 font-bold text-2xl mb-12 w-8/12">Set Goals, Stay Accountable, Succeed Together</h4>
+                <div className="flex flex-col  m-0 items-center rounded-md border-slate-300 border-solid border-2 text-slate-300 p-10">
+                    <h2 className="w-full font-bold mb-5 text-3xl">Sign In</h2>
+                    <div className="flex flex-col w-full">
+                        <label htmlFor="email" className="w-full text-sm text-start mb-0">Email</label>
                         <input id='email' type="email" onChange={(e)=>{setEmail(e.target.value)}} required className="flex items-center w-full px-3 py-2 mr-2 text-sm font-medium outline-none text-slate-800 focus:bg-grey-400 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"/>
                         <label htmlFor='password' className="text-sm text-start mt-3 mb-0">Password</label>
                         <input type="password" id="password" onChange={(e)=>{e.target.value}} required className="flex items-center w-full px-3 py-2 mr-2 text-sm font-medium outline-none text-slate-800 focus:bg-grey-400 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"/>
                         <input type="submit" id="submitLoginButton" value="Sign In" className="bg-slate-500 w-full px-6 py-5 mb-5 text-sm font-bold leading-none text-white transition duration-300 md:w-96 rounded-2xl hover:bg-purple-blue-600 focus:ring-4 focus:ring-purple-blue-100 bg-purple-blue-500"/>
-                        <input type="button" id="linkToRegister" value='Not registered yet? Create an Account' onClick={()=> navigate("/register")} className="underline underline-offset-1"/>
+                        <input type="button" id="linkToRegister" value='Not registered yet? Create an Account' onClick={()=> navigate("/register")} className="w-full underline underline-offset-1"/>
                     </div>
                 </div>
             </form>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import { useNavigate } from "react-router-dom";
+import Loader from "../components/Loader";
 
 const Register = () => {
     const navigate = useNavigate();
@@ -7,11 +8,12 @@ const Register = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState({});
+    const [loader, setLoader] = useState(false);
 
     const getData = async (e) => {
         e.preventDefault();
         try {
-                //loader insert here
+            setLoader(!loader);
             //set data for Post    
             const requestData = {
                 method: 'POST',
@@ -43,29 +45,30 @@ const Register = () => {
         } catch(error) {
             console.log({error: error.message});
         } finally {
-            //loader
+            setLoader(false);
         }
     }
     return(
         <>
-        <div className="text-slate-300 flex flex-row">
-            <div className="w-6/12  min-h-screen bg-slate-950 min-w-screen flex flex-col justify-between" >
+        <div className="text-slate-300 flex lg:flex-row">
+            <div className="max-lg:invisible w-6/12 max-lg:w-0  min-h-screen bg-slate-950 min-w-screen flex flex-col justify-between" >
             <h2 className="font-bold text-2xl text-start pl-5 pt-5 mt-5 ml-5 w-8/12">Achieve More Together. Your DailyGoals, Our Common Mission</h2>
             <h2 className="font-bold mb-5 ml-5 text-2xl self-start pl-5 pb-5">taskTogether</h2>
-
             </div>
-            <form onSubmit={getData} className="bg-slate-800 w-6/12 m-0 min-h-screen min-w-screen flex justify-center items-center">
-                <div className="flex flex-col  m-0 items-center rounded-md border-slate-300 border-solid border-2 text-slate-300 p-12">
-                    <h2 className="font-bold mb-5 text-3xl">Create Account</h2>
-                    <div className="flex flex-col">
-                        <label htmlFor="name" className="text-sm text-start mb-0">Name</label>
+            <form onSubmit={getData} className="bg-slate-800 w-6/12 max-lg:w-screen m-0 min-h-screen min-w-screen flex max-lg:flex-col justify-center items-center">
+                <h2 className=" lg:invisible lg:w-0 font-bold  text-2xl mb-4">taskTogether</h2>
+                <h4 className=" lg:invisible lg:w-0 font-bold text-2xl mb-12 w-8/12">Achieve More Together. Your DailyGoals, Our Common Mission</h4>
+                <div className="flex flex-col  m-0 items-center rounded-md border-slate-300 border-solid border-2 text-slate-300 p-10">
+                    <h2 className="font-bold mb-5 text-3xl w-full">Create Account</h2>
+                    <div className="flex flex-col w-full">
+                        <label htmlFor="name" className="w-full text-sm text-start mb-0">Name</label>
                         <input id="name" onChange={(e)=>{setName(e.target.value)}} required className="flex items-center w-full px-3 py-2 mr-2 text-sm font-medium outline-none text-slate-800 focus:bg-grey-400 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"/>
                         <label htmlFor="email" className="text-sm text-start mt-3 mb-0">Email</label>
                         <input id="email" type="email" onChange={(e)=>{setEmail(e.target.value)}} required className="flex items-center w-full px-3 py-2 mr-2 text-sm font-medium outline-none text-slate-800 focus:bg-grey-400 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"/>
                         <label htmlFor="password" className="text-sm text-start mt-3 mb-0">Password</label>
                         <input type="password" id="password" onChange={(e)=>{setPassword(e.target.value)}} required className="flex items-center w-full px-3 py-2 mr-2 text-sm font-medium outline-none text-slate-800 focus:bg-grey-400 mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-2xl"/>
                         <input type="submit" id="registerSubmitButton" value="Create Account" className="bg-slate-500 w-full px-6 py-5 mb-5 text-sm font-bold leading-none text-white transition duration-300 md:w-96 rounded-2xl hover:bg-purple-blue-600 focus:ring-4 focus:ring-purple-blue-100 bg-purple-blue-500" />
-                        <input type="button" id="linkToLogin" value='Already registered? Sign In' onClick={()=> navigate("/login")} className="underline underline-offset-1"/>
+                        <input type="button" id="linkToLogin" value='Already registered? Sign In' onClick={()=> navigate("/login")} className="w-full underline underline-offset-1"/>
                     </div>
                 </div>    
             </form>
