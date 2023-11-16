c87bc639dd4640f51952f9dc8162ce526cd38bc9
Mateo Santa <mateo.santamanrique@gmail.com>
2023-11-16 12:00:06 +0100

    homepage

--- client/src/components/home/Footer.jsx
+++ client/src/components/home/Footer.jsx
@@ -5,10 +5,12 @@ const Footer = () => {
 
     return(
         <footer className="bg-slate-300 rounded-lg shadow">
-    <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span className="text-sm text-slate-950 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">taskTogether™</a>. All Rights Reserved.
-    </span>
-    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-slate-950 dark:text-gray-400 sm:mt-0">
+    <div className=" flex w-full mx-auto max-w-screen-xl p-4 justify-between">
+        <div>
+            <span className="text-sm text-slate-950 sm:text-center dark:text-gray-400">© 2023 </span><button onClick={navigate("/homepage")} className="hover:underline">taskTogether™</button><span> All Rights Reserved.</span>
+        </div>
+    <div></div>
+    <ul className="flex items-center text-sm font-medium text-slate-950 dark:text-gray-400 mt-0">
         <li>
             <button href="#" className="hover:underline me-4 md:me-6" onClick={()=>navigate("/login")}>Sign In</button>
         </li>
@@ -16,6 +18,7 @@ const Footer = () => {
             <button href="#" className="hover:underline" onClick={()=>navigate("/register")}>Create Account</button>
         </li>
     </ul>
+
     </div>
 </footer>
 
