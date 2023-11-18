06a0dbc98033b98d5767f197b531c315e4b6d959
Mateo Santa <mateo.santamanrique@gmail.com>
2023-11-18 14:14:07 +0100

    socket deploy link insertion

--- server/db/socket.js
+++ server/db/socket.js
@@ -3,7 +3,7 @@ import { Server } from 'socket.io';
 const initializeSocket = (server) => {
 const io = new Server(server, {
 	cors:{
-		origin: "http://localhost:5173",
+		origin: "https://tasktogether.onrender.com",
         methods: ['GET', 'POST'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
