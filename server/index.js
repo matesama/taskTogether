420378be74304732c295d108cc7071583be29b0b
Christwelve <meng.christian28@gmail.com>
2024-01-17 14:01:44 +0100

    copied all socket fucntions into server index.js

--- server/index.js
+++ server/index.js
@@ -18,19 +18,78 @@ const server = http.createServer(app);
 
 const io = initializeSocket(server);
 app.use(
-    cors({
-      origin: "http://localhost:5173",
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-      allowedHeaders: [
-        'Content-Type',
-        'Authorization',
-        'Access-Control-Allow-Origin',
-        'Access-Control-Allow-Credentials',
-      ],
-      credentials: true,
-    })
-  );
+  cors({
+    origin: "http://localhost:5173",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Access-Control-Allow-Origin',
+      'Access-Control-Allow-Credentials',
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  if (!users.some((user) => user.userId === userId)) {
+    users.push({ userId, socketId });
+  }
+}
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+}
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+}
+
+
+io.on('connection', (socket) => {
+
+  // when connect
+  console.log('a user connected');
+
+
+  // take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+    console.log('added, Users: ', users);
+
+  })
+
+  // send and get message
+  socket.on('sendMessage', ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    if (user && user.socketId) {
+      io.to(user.socketId).emit("getMessage", {
+        senderId,
+        text,
+      });
+    } else {
+      console.log('User or user.socketId is undefined:', user);
+    }
+  })
+
+  // new conversation
+  socket.on('newConversation', () => {
+    io.emit('newConversation');
+    console.log('newConversation');
+  })
+
+  // when logout
+  socket.on("logout", () => {
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+    console.log('a user logged out, still logged in users: ', users);
+  });
+});
+
 
 app.use('/api/test', testRouter);
 app.use('/api/auth', authRouter);
